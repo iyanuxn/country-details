d3759cd964fce3e000de1a507ccd0f05cfd9386f
Iyanu <iyanuxn@gmail.com>
2023-08-12 17:43:51 +0100

    dark mode toggle working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import { BsMoon } from "react-icons/bs";
 
 const Navbar = () => {
+  const [dark, setDark] = useState(false);
+  const toggleDark = () => {
+    setDark(!dark);
+  };
+
   return (
-    <div className="flex justify-between items-center h-16 shadow-md px-36">
-      <span className="text-2xl font-bold">Where in the world?</span>
-      <span>Dark</span>
+    <div className={dark ? "dark" : ""}>
+      <div className="transition duration-300 ease-in-out flex justify-between items-center h-16 shadow-md px-16 text-slate-800 dark:bg-slate-950 dark:text-white">
+        <span className="text-2xl font-bold">Where in the world?</span>
+        <button
+          className="flex items-center gap-3 font-semibold"
+          onClick={toggleDark}
+        >
+          <BsMoon />
+          <span>Dark Mode</span>
+        </button>
+      </div>
     </div>
   );
 };
