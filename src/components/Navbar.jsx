7c6119e4175c7a187a259a64af0c3b23a4851fe6
Iyanu <iyanuxn@gmail.com>
2023-08-14 13:22:09 +0100

    Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { BsMoon } from "react-icons/bs";
 
 const Navbar = ({ toggleDark }) => {
   return (
-    <div className="transition duration-300 ease-in-out flex justify-between items-center h-16 shadow-md relative px-16 py-10 bg-white text-slate-800 dark:bg-slate-700 dark:text-white">
-      <span className="text-2xl font-bold">Where in the world?</span>
+    <div className="transition duration-300 ease-in-out flex flex-row justify-between items-center h-16 shadow-md relative px-5 md:px-16 py-10 bg-white text-slate-800 dark:bg-slate-700 dark:text-white">
+      <span className="md:text-2xl text-sm font-bold">Where in the world?</span>
       <button
-        className="flex items-center gap-3 font-semibold"
+        className="flex items-center gap-3 font-semibold text-sm md:text-base"
         onClick={toggleDark}
       >
         <BsMoon />
